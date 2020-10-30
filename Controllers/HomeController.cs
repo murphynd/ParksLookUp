29bfe5c67662a54830fd639d0ad25b1845e40148
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-30 16:25:44 -0700

    Views: I wanted to add a simple sign up te get your tolken to use the API but I could not figure it out today.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Treat.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public ActionResult Signup()
+    {
+      return View();
+    }
+
+  }
+}
